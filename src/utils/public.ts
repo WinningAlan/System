ec33a5ca73f8c715a1ff9e1ee4b8724420a16a00
sunshibing123 <952217382@qq.com>
2021-04-13 18:38:51 +0800

    iss53bug

--- src/utils/public.ts
+++ src/utils/public.ts
@@ -1,9 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2021-04-01 13:53:08
- * @LastEditTime: 2021-04-01 13:53:09
+ * @LastEditTime: 2021-04-13 18:37:46
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \System\src\utils\public.ts
  */
 //
+export const setArr = (arr:Array<any>)=>{
+    return arr;
+}
