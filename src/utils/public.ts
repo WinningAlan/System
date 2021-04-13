e3df4cf6e6844e785b728ecbf07fcb2b4aad8e2c
sunshibing123 <952217382@qq.com>
2021-04-13 19:02:42 +0800

    修改

--- src/utils/public.ts
+++ src/utils/public.ts
@@ -1,7 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2021-04-01 13:53:08
- * @LastEditTime: 2021-04-13 18:27:14
+ * @LastEditTime: 2021-04-13 18:57:09
+ * @LastEditTime: 2021-04-13 18:37:46
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \System\src\utils\public.ts
@@ -9,4 +10,7 @@
 //
 export const zero = (val: string | number): string => {
     return val > 9 ? val + "" : "0" + val;
+}
+export const setArr = (arr:Array<any>)=>{
+    return arr;
 }
